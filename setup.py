682ac76f62f3ab5533ba2df19e9381f2bb4b217f
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2017-05-22 19:33:38 +0200

    [Fix] Detect Python 2 and install `enum34` in that case

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+import sys
+
 from distutils.core import setup
 
-setup(name='extras_mongoengine',
-      version='0.1',
-      description='MongoEngine Extras - Field Types and any other wizardry.',
-      url='https://github.com/MongoEngine/extras-mongoengine/',
-      install_requires=['mongoengine>=0.8.6'],
-      packages=['extras_mongoengine'],
-     )
+# If we're on Python 2
+install_requires = ['mongoengine>=0.8.6']
+if sys.version_info[0] == 2:
+    # - then we also require `enum34`
+    install_requires += [
+        'enum34',
+    ]
+
+setup(
+    name='extras_mongoengine',
+    version='0.1',
+    description='MongoEngine Extras - Field Types and any other wizardry.',
+    url='https://github.com/MongoEngine/extras-mongoengine/',
+    install_requires=install_requires,
+    packages=['extras_mongoengine'],
+)
